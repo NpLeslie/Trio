3e61ce9ffa1387474a05f2382afa694329e76def
npleslie <npleslie@mail.csuchico.edu>
2019-11-13 16:28:22 -0800

    added functions to order input numbers

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
+void reorder(int&, int&, int&); 
 
 int main()
 {
@@ -13,7 +14,7 @@ int main()
 
 
   // <-- ADD YOUR FUNCTION CALL HERE
-
+  reorder(red, green, blue);
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
@@ -22,4 +23,54 @@ int main()
   //...END OF "DO NOT CHANGE" AREA
 }
 
-// <-- ADD YOUR FUNCTION DEFINITON HERE
+// <-- ADD YOUR FUNCTION DEFINITON HERE 
+
+void reorder(int& red, int& green, int& blue)
+{
+  int high=0;
+  int med=0;
+  int low=0;
+
+  if(red>green && red>blue)
+  {
+    high=red;
+  }
+  else if(red<green && red<blue)
+  {
+    low=red;
+  }
+  else
+  {
+    med=red;
+  }
+
+  if(green>red && green>blue)
+  {
+    high=green;
+  } 
+  else if(green<red && green<blue)
+  {
+    low=green;
+  }
+  else
+  {
+    med=green;
+  }
+
+  if(blue>red && blue>green)
+  {
+    high=blue;
+  }
+  else if(blue<red && blue<green) 
+  {
+    low=blue;        
+  }
+  else
+  {
+    med=blue;
+  }
+
+  red=high;
+  green=med;
+  blue=low;
+}
